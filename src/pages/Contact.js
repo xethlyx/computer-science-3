7a50fb9ac56c66938f805c8229fa7cf899c4a583
xethlyx <46338199+xethlyx@users.noreply.github.com>
2019-09-13 14:22:53 -0400

    Updated minor pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 
 class Index extends Component {
   state = {
-    
+    video : false
   };
+
+  showVideo = () => {
+    if (this.state.video) {
+      this.setState({
+        video: false
+      });
+    } else {
+      this.setState({
+        video: true
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Contact</h2>
-				<p>still testing
+				<p>Do you have problems with your instant RAM download? Simply contact us at <a href="mailto:example@example.com">example@example.com</a>. We'll have matters resolved within 9000 hours.
+        <br /><br /><a href="#video" onClick={this.showVideo}>Learn More</a><br /><br />
+        { this.state.video && 
+          <iframe title="youtube video" width="1280" height="720" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        }
         </p>
       </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ class Index extends Component {
 				<p>Often times, when a lot of apps are crashing, it'll be because you're out of RAM!</p>
         
         <h2>Want to download?</h2>
-        <p>First, you'll have to see <a href="http://ismycomputeron.com/" target="_blank" rel="noopener">if your computer is on</a>. Once you've confirmed that your computer is on, head over to the Downloads page and see how much RAM you want to download. More RAM means more power as the computer has to keep sending electricity to keep the data in memory, so choose appropiately!</p>
+        <p>First, you'll have to see <a href="http://ismycomputeron.com/" target="_blank" rel="noopener noreferrer">if your computer is on</a>. Once you've confirmed that your computer is on, head over to the Downloads page and see how much RAM you want to download. More RAM means more power as the computer has to keep sending electricity to keep the data in memory, so choose appropiately!</p>
       </div>
     )
   }
